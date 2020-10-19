c3e3ed9daaec79c2f0e8c324408178905ef3c250
Alejandro <alejandroup170@gmail.com>
2020-10-18 21:47:42 -0500

    Update Program.py

---
+++ program.py
@@ -0,0 +1,48 @@
+import fileinput
+
+# Inicializa vector S
+def initVectS(vectS):
+    for x in range(0,256):
+        vectS.append(x)
+    return vectS
+
+# Algoritmo KSA
+def KSA(vectS, k, mess):
+    j = 0
+    lenght = len(k)
+    for i in range(0,256):
+        j = (j + vectS[i] + int(k[i%lenght]))%256
+        vectS[i], vectS[j] = vectS[j], vectS[i]
+    return vectS
+
+# Algoritmo PRGA y cifrado
+def PRGA(vectS, mess):
+    i, j, k = 0, 0, 0
+    out = ""
+    while len(mess)>0:
+        i = (i + 1)%256
+        j = (j + vectS[i])%256
+        vectS[i], vectS[j] = vectS[j], vectS[i]
+        k = S[((vectS[j] + vectS[i])%256)]
+        out += str((int(mess)^(k))%256)
+        mess = mess[1:]
+    return out
+    
+
+# Lee entradas
+lines = []
+for line in fileinput.input():
+    lines.append(line)
+
+# Obtener las llaves y texto plano, limpiando el input
+key = lines[0].replace("\n","")
+key = key.replace(" ","")
+text = lines[1].replace(" ","") 
+text = text.replace("\n","")
+
+# Algoritmo main
+S = []
+S = initVectS(S)
+S = KSA(S, key, text)
+cif = PRGA(S, text)
+print(cif)
